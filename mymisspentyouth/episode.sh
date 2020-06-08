2fb02c45b569174b37632306f6539a8878ccd6aa
Al <alan@nuna.com>
2020-06-07 22:13:18 -0700

    update episode.sh

--- mymisspentyouth/episode.sh
+++ mymisspentyouth/episode.sh
@@ -3,11 +3,13 @@ EPISODE=${1}
 TITLE=${2}
 
 usage(){
-    echo "${0} <episode> <title>"
+    echo "${0} <episode> <title> <tracklist>"
     echo "e.g. ${0} 005 \"Speckled Frogs\""
 }
 
 [[ ${1} ]] || usage
 [[ ${2} ]] || usage
+[[ ${3} ]] || usage
 
-sed -e "s/%%%000%%%/${EPISODE}/g" -e "s/%%%TITLE%%%/${TITLE}/g"
+
+sed -e "s/%%%000%%%/${EPISODE}/g" -e "s/%%%TITLE%%%/${TITLE}/g" template-episode.txt
